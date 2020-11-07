4df2b18801e774c6ec13660aa92d058a9c1d744c
aioann01 <andreas.ioannou.gewrgiou@gmail.com>
2020-11-07 18:00:57 +0200

    test

---
+++ accuracyStatistics.py
@@ -0,0 +1,72 @@
+import requests
+import sys
+import time
+import json
+#Constants
+HTTPS_PROTOCOL = "http://"
+WORKLOAD_GENERATOR_PORT = str(8091)
+WORKLOAD_GENERATOR_RESOURCE = "/workloadGenerator"
+HTTP_TESTING_RESOURCE = "/testing"
+HTTP_SERVER_PORT = str(8095)
+host =  sys.argv[1]
+waitTime =  int(sys.argv[2])
+messageIntervalTime = int(sys.argv[3])
+workloadGeneratorStartURL = HTTPS_PROTOCOL + host + ":" + WORKLOAD_GENERATOR_PORT + WORKLOAD_GENERATOR_RESOURCE + "/start"
+workloadGeneratorRestartURL = HTTPS_PROTOCOL + host + ":" + WORKLOAD_GENERATOR_PORT + WORKLOAD_GENERATOR_RESOURCE + "/restart"
+workloadGeneratorStopURL = HTTPS_PROTOCOL + host + ":" + WORKLOAD_GENERATOR_PORT + WORKLOAD_GENERATOR_RESOURCE + "/stop"
+httpServerResetURL =  HTTPS_PROTOCOL + host + ":" + HTTP_SERVER_PORT + HTTP_TESTING_RESOURCE + "/reset"
+httpServerPrepareURL = HTTPS_PROTOCOL + host + ":" + HTTP_SERVER_PORT + HTTP_TESTING_RESOURCE + "/prepare"
+httpServerprintStatisticsURL =  HTTPS_PROTOCOL + host + ":" + HTTP_SERVER_PORT + HTTP_TESTING_RESOURCE + "/printStatistics"
+
+
+#PAYLOADS
+EMPTY_JSON = {}
+httpPreparePaylaod = {'sensorPrototypeName':'panel',"messageIntervalTime":messageIntervalTime}
+def printAvg():
+    with open('temperature_AccuracyEvaluation,json') as json_file:
+        data = json.load(json_file)
+        print("Avg delay time:", data['avgSensorMessageDelay'])
+
+
+
+
+#Main
+
+sensorsNumber = 10
+
+#while sensorsNumber < 100:
+prepareResponse = requests.post( url = httpServerPrepareURL, json = httpPreparePaylaod)
+httpServerPrepareHttpResponseCode = prepareResponse.status_code 
+print(" prepareTesting response:", prepareResponse.text, "and prepareTesting Http Response Code:", httpServerPrepareHttpResponseCode, "\n")
+
+
+
+workloadGeneratorRestartResponse = requests.post( url = workloadGeneratorRestartURL, json = EMPTY_JSON)
+workloadGeneratorRestartHttpResponseCode = workloadGeneratorRestartResponse.status_code
+print(" workloadGeneratorRetart response:", workloadGeneratorRestartResponse.text, "and workloadGeneratorRestart Http Response Code:", workloadGeneratorRestartHttpResponseCode, "\n")
+    
+
+
+
+
+#workloadGeneratorStartResponse = requests.post( url = workloadGeneratorStartURL, json = EMPTY_JSON)
+#workloadGeneratorStartHttpResponseCode = workloadGeneratorStartResponse.status_code
+#print(" workloadGeneratorStart response:", workloadGeneratorStartResponse.text, "and workloadGeneratorStart Http Response Code:", workloadGeneratorStartHttpResponseCode, "\n")
+    
+time.sleep(waitTime*60)
+
+workloadGeneratorStopResponse = requests.post( url = workloadGeneratorStopURL, json = EMPTY_JSON)
+workloadGeneratorStopHttpResponseCode = workloadGeneratorStopResponse.status_code
+print(" workloadGeneratorStop response:", workloadGeneratorStopResponse.text, "and workloadGeneratorStop Http Response Code:", workloadGeneratorStopHttpResponseCode, "\n")
+ 
+
+printStatisticsResponse = requests.post( url = httpServerprintStatisticsURL, json = EMPTY_JSON)
+httpServerprintStatisticsHttpResponseCode = printStatisticsResponse.status_code 
+print(" printStatistics response:", printStatisticsResponse.text, "and printStatistics Http Response Code:", httpServerprintStatisticsHttpResponseCode, "\n")
+
+resetResponse = requests.post( url = httpServerResetURL, json = EMPTY_JSON)
+httpServerResetHttpResponseCode = resetResponse.status_code 
+print(" resetTesting response:", resetResponse.text, "and resetTesting Http Response Code:", httpServerResetHttpResponseCode, "\n")
+
+
+#printAvg()
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+
+java -jar home/WorkloadGenerator-1.0.0-SNAPSHOT.jar
+RUN apk add --no-cache bash
