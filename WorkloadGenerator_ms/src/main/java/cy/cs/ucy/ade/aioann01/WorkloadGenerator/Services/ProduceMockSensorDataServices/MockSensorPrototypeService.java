e3fd35d39bce2c3ec9c6e154ed04d88e1df7eed1
aioann01 <andreas.ioannou.gewrgiou@gmail.com>
2021-01-16 22:47:16 +0200

    Clean Services for repositories

--- WorkloadGenerator_ms/src/main/java/cy/cs/ucy/ade/aioann01/WorkloadGenerator/Services/ProduceMockSensorDataServices/MockSensorPrototypeService.java
+++ WorkloadGenerator_ms/src/main/java/cy/cs/ucy/ade/aioann01/WorkloadGenerator/Services/ProduceMockSensorDataServices/MockSensorPrototypeService.java
@@ -1,35 +1,28 @@
 package cy.cs.ucy.ade.aioann01.WorkloadGenerator.Services.ProduceMockSensorDataServices;
 
-import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.MockSensorData.GetMockSensorPrototypesResponse;
+import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.MockSensors.GetMockSensorPrototypesResponse;
 import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.Http.Exchange;
 import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.SensorPrototype.MockSensorPrototype;
 import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Repository.MockSensorPrototypeRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 import static cy.cs.ucy.ade.aioann01.WorkloadGenerator.Utils.FrameworkConstants.*;
-import static cy.cs.ucy.ade.aioann01.WorkloadGenerator.Utils.FrameworkConstants.HTTP_NOT_FOUND;
 
 @Service
-public class MockSensorPrototypeService {
+public class MockSensorPrototypeService{
 
     @Autowired
     public MockSensorPrototypeRepository mockSensorPrototypeRepository;
 
-    private static final Logger log= LoggerFactory.getLogger(MockSensorPrototypeService.class);
-
-    public void findMockSensorPrototypeByName(Exchange exchange,String mockSensorPrototypeName){
-        MockSensorPrototype mockSensorPrototype= mockSensorPrototypeRepository.getMockSensorPrototypeByMockSensorPrototypeName(mockSensorPrototypeName);
-        if(mockSensorPrototype!=null)
+    public void findMockSensorPrototypeByName(Exchange exchange, String mockSensorPrototypeName){
+        MockSensorPrototype mockSensorPrototype = mockSensorPrototypeRepository.getMockSensorPrototypeByMockSensorPrototypeName(mockSensorPrototypeName);
+        if(mockSensorPrototype != null)
             exchange.setBody(mockSensorPrototype);
         else{
-            exchange.setProperty(ERROR_MESSAGE_TYPE,VALIDATION_ERROR);
-            exchange.setProperty(ERROR_MESSAGE,"MockSensorPrototype with name: "+mockSensorPrototypeName +" not found");
-            exchange.setHttpStatus(HTTP_NOT_FOUND);
+            Utils.setNotFoundErrorOnExchange(exchange, "MockSensorPrototype with name: "+mockSensorPrototypeName +" not found", VALIDATION_ERROR);
         }
 
     }
@@ -39,6 +32,7 @@ public class MockSensorPrototypeService {
         return mockSensorPrototypeRepository.getMockSensorPrototypeByMockSensorPrototypeName(mockSensorPrototypeName);
     }
 
+
     public void retrieveAllMockSensorPrototypes(Exchange exchange) {
        GetMockSensorPrototypesResponse response = new GetMockSensorPrototypesResponse();
        response.setMockSensorPrototypes(mockSensorPrototypeRepository.getAllMockSensorPrototypes());
@@ -46,27 +40,33 @@ public class MockSensorPrototypeService {
        exchange.setBody(response);
     }
 
+
     public void addMockSensorPrototype(MockSensorPrototype mockSensorPrototype) {
         mockSensorPrototypeRepository.addMockSensorPrototype(mockSensorPrototype);
     }
 
-    public void addAllMockSensorPrototype(List<MockSensorPrototype> mockSensorPrototypes) {
+
+    public void addAllMockSensorPrototypes(List<MockSensorPrototype> mockSensorPrototypes) {
         mockSensorPrototypeRepository.addAllMockSensorPrototypes(mockSensorPrototypes);
     }
+
 
     public void deleteMockSensorPrototype(String mockSensorPrototypeName) {
         mockSensorPrototypeRepository.deleteMockSensorPrototype(mockSensorPrototypeName);
     }
 
+
     public void deleteAll(){
         mockSensorPrototypeRepository.deleteAll();
     }
 
-    public void editMockSensorsNumberForMockSensorPrototype(int mockSensorCount,String mockSensorPrototypeName){
-        MockSensorPrototype mockSensorPrototype= mockSensorPrototypeRepository.getMockSensorPrototypeByMockSensorPrototypeName(mockSensorPrototypeName);
-        if(mockSensorPrototype!=null) {
+
+    public void editMockSensorsNumberForMockSensorPrototype(int mockSensorCount, String mockSensorPrototypeName){
+        MockSensorPrototype mockSensorPrototype = mockSensorPrototypeRepository.getMockSensorPrototypeByMockSensorPrototypeName(mockSensorPrototypeName);
+        if(mockSensorPrototype != null) {
             mockSensorPrototype.setSensorsQuantity(mockSensorCount);
-            mockSensorPrototypeRepository.updateMockSensorPrototype(mockSensorPrototypeName,mockSensorPrototype);
+            mockSensorPrototypeRepository.updateMockSensorPrototype(mockSensorPrototypeName, mockSensorPrototype);
         }
     }
+
 }
--- WorkloadGenerator_ms/src/main/java/cy/cs/ucy/ade/aioann01/WorkloadGenerator/Services/ProduceMockSensorDataServices/MockSensorService.java
+++ WorkloadGenerator_ms/src/main/java/cy/cs/ucy/ade/aioann01/WorkloadGenerator/Services/ProduceMockSensorDataServices/MockSensorService.java
@@ -1,11 +1,10 @@
 package cy.cs.ucy.ade.aioann01.WorkloadGenerator.Services.ProduceMockSensorDataServices;
 
-import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.MockSensorData.GetSensorsResponse;
+import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.MockSensors.GetSensorsResponse;
 import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.Http.Exchange;
-import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.MockSensorData.MockSensor;
+import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Model.MockSensors.MockSensor;
 import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Repository.MockSensorRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import cy.cs.ucy.ade.aioann01.WorkloadGenerator.Utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -19,45 +18,49 @@ public class MockSensorService {
     @Autowired
     MockSensorRepository mockSensorRepository;
 
-    private static final Logger log= LoggerFactory.getLogger(MockSensorService.class);
 
     public void addAllMockSensors(List<MockSensor> mockSensors){
         mockSensorRepository.addAllMockSensors(mockSensors);
     }
 
+
     public List<MockSensor> getAllMockSensors(){
-        return  mockSensorRepository.getAllMockSensors();
+        return mockSensorRepository.getAllMockSensors();
     }
 
-    public void  findMockSensorById(Exchange exchange,String mockSensorId){
+
+    public void findMockSensorById(Exchange exchange, String mockSensorId){
         MockSensor mockSensor = mockSensorRepository.getMockSensorByMockSensorId(mockSensorId);
-        if(mockSensor !=null)
+        if(mockSensor != null)
             exchange.setBody(mockSensor);
         else{
-            exchange.setProperty(ERROR_MESSAGE_TYPE,VALIDATION_ERROR);
-            exchange.setProperty(ERROR_MESSAGE,"MockSensor with id: "+mockSensorId +" not found");
-            exchange.setHttpStatus(HTTP_NOT_FOUND);}
+            Utils.setNotFoundErrorOnExchange(exchange, "MockSensor with id: "+mockSensorId +" not found", VALIDATION_ERROR);
+        }
     }
 
 
-    public void  retrieveAllMockSensors(Exchange exchange){
-        GetSensorsResponse response=new GetSensorsResponse();
+    public void retrieveAllMockSensors(Exchange exchange){
+        GetSensorsResponse response = new GetSensorsResponse();
         response.setMockSensors(mockSensorRepository.getAllMockSensors());
         response.setTotalCount(response.getMockSensors().size());
         exchange.setBody(response);
     }
 
+
     public void addMockSensor(MockSensor mockSensor){
         mockSensorRepository.addMockSensor(mockSensor);
     }
+
 
     public void deleteMockSensor(String mockSensorId){
         mockSensorRepository.deleteMockSensor(mockSensorId);
     }
 
-    public void deleteMockSensorsOfMockSensorPorotype(String mockSensorPrototypeName){
+
+    public void deleteMockSensorsOfMockSensorPrototype(String mockSensorPrototypeName){
         mockSensorRepository.deleteMockSensorsOfMockSensorPrototype(mockSensorPrototypeName);
     }
+
 
     public void deleteAll(){
         mockSensorRepository.deleteAll();
